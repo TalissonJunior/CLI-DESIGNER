aa2c537550e965ed587b20b6055d7ab195f287ca
Talisson junior <juniortalisson16@gmail.comr>
2019-10-25 23:09:56 -0300

    UPDATE

--- src/app/minimap.ts
+++ src/app/minimap.ts
@@ -1,10 +1,13 @@
 import { WorkSpace } from './workspace';
 
+/**
+ * Creates the minimap for workspace
+ */
 export class Minimap {
   workSpace: WorkSpace;
   self: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
   svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
-
+  frame: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
   options = {
     scale: 16
   };
@@ -19,10 +22,23 @@ export class Minimap {
     const containerHeight = this.workSpace.svg.attr('height');
     const containerWidth = this.workSpace.svg.attr('width');
 
-    this.workSpace.self.append('svg').attrs({
+    const minimapWidth = parseInt(containerWidth) / this.options.scale;
+    const minimapHeight = parseInt(containerHeight) / this.options.scale;
+
+    const minimapFramWidth = minimapWidth / (this.options.scale / 6);
+    const minimapFramHeight = minimapHeight / (this.options.scale / 6);
+
+    this.svg = this.workSpace.self.append('svg').attrs({
       class: 'minimap minimap-bottom-right',
-      height: parseInt(containerHeight) / this.options.scale,
-      width: parseInt(containerWidth) / this.options.scale
+      height: minimapHeight,
+      width: minimapWidth
+    });
+
+    this.svg.append('rect').attrs({
+      class: 'minimap-viewport',
+      height: minimapFramHeight,
+      width: minimapFramWidth,
+      stroke: '#aaaaaa'
     });
   }
 }
