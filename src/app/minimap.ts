2720af665c4509dcb77ef4930f5180d7357abe44
Talisson junior <juniortalisson16@gmail.comr>
2019-10-29 23:58:08 -0300

    UPDATE

--- src/app/minimap.ts
+++ src/app/minimap.ts
@@ -1,4 +1,5 @@
 import { WorkSpace } from './workspace';
+import * as d3 from 'd3';
 
 /**
  * Creates the minimap for workspace
@@ -7,9 +8,10 @@ export class Minimap {
   workSpace: WorkSpace;
   self: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
   svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
-  frame: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
+  zoomViewport: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
   options = {
-    scale: 16
+    scale: 16,
+    minimapZoomScale: 0.3
   };
 
   constructor(workspace: WorkSpace) {
@@ -22,23 +24,65 @@ export class Minimap {
     const containerHeight = this.workSpace.svg.attr('height');
     const containerWidth = this.workSpace.svg.attr('width');
 
+    // Calculate height
     const minimapWidth = parseInt(containerWidth) / this.options.scale;
     const minimapHeight = parseInt(containerHeight) / this.options.scale;
 
-    const minimapFramWidth = minimapWidth / (this.options.scale / 6);
-    const minimapFramHeight = minimapHeight / (this.options.scale / 6);
-
+    // create minimap container
     this.svg = this.workSpace.self.append('svg').attrs({
       class: 'minimap minimap-bottom-right',
       height: minimapHeight,
       width: minimapWidth
     });
 
-    this.svg.append('rect').attrs({
+    this.createZoomViewPort(this.svg);
+  }
+
+  /**
+   * Create minimap zoom view port
+   */
+  createZoomViewPort(
+    svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>
+  ): void {
+    const svgHeight = svg.attr('height');
+    const svgWidth = svg.attr('width');
+
+    this.zoomViewport = svg.append('rect').attrs({
       class: 'minimap-viewport',
-      height: minimapFramHeight,
-      width: minimapFramWidth,
+      height: svgHeight,
+      width: svgWidth,
+      transform: 'translate(0,0)scale(' + this.options.minimapZoomScale + ')',
       stroke: '#aaaaaa'
     });
+
+    var dragPosX = 0;
+    var dragPosY = 0;
+
+    // Add drag behavior
+    const drag = d3.drag().on('drag', () => {
+      dragPosX += d3.event.dx;
+      dragPosY += d3.event.dy;
+
+      this.zoomViewport.attr(
+        'transform',
+        'translate(' +
+          dragPosX +
+          ',' +
+          dragPosY +
+          ')' +
+          'scale(' +
+          this.options.minimapZoomScale +
+          ')'
+      );
+
+      var translate = [
+        -dragPosX * this.options.scale,
+        -dragPosY * this.options.scale
+      ];
+
+      this.workSpace.svg.attr('transform', 'translate(' + translate + ')');
+    });
+
+    this.zoomViewport.call(drag);
   }
 }
