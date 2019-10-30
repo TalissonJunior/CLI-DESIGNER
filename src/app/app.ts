afc8138cc359018e90b60159b5d45e1c0c3e0761
Bruno Gabriel <brunogabriel1701@hotmail.com>
2019-10-30 16:56:59 -0300

    BUGFIX: Adjusts

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,9 +9,27 @@ class App {
 
   constructor(container: string) {
     this.workspace = new WorkSpace(container);
-    this.minimap = new Minimap(this.workspace);
+
+    const mini = this.createMinimapSvg();
+    // initialize the minimap, passing needed references
+    const minimap = new Minimap(mini)
+      .setHost(mini)
+      .setTarget(this.workspace.svg)
+      .setMinimapScale(0.1)
+      .setX(3840 + 20)
+      .setY(20);
+
+    this.workspace.svg.call(minimap);
 
     this.init();
+  }
+
+  createMinimapSvg() {
+    return this.workspace.self.append('svg').attrs({
+      class: 'minmap',
+      height: 2400,
+      width: 3840
+    });
   }
 
   private init() {}
--- src/app/minimap.ts
+++ src/app/minimap.ts
@@ -1,88 +1,154 @@
-import { WorkSpace } from './workspace';
 import * as d3 from 'd3';
 
 /**
- * Creates the minimap for workspace
+ * https://codepen.io/billdwhite/pen/lCAdi
+ * Minimap
  */
 export class Minimap {
-  workSpace: WorkSpace;
-  self: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
-  svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
-  zoomViewport: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
-  options = {
-    scale: 16,
-    minimapZoomScale: 0.3
-  };
+  minimapScale = 0.15;
+  host = null;
+  base = null;
+  target;
+  zoom: d3.ZoomBehavior<Element, unknown>;
+  container;
+  frame;
+  width = 0;
+  height = 0;
+  x = 0;
+  y = 0;
 
-  constructor(workspace: WorkSpace) {
-    this.workSpace = workspace;
+  constructor(selection: d3.Selection<d3.BaseType, unknown, HTMLElement, any>) {
+    this.base = selection;
 
-    this.create();
+    this.zoom = d3.zoom().scaleExtent([0.5, 5]);
+    this.zoom.on('zoom', this.zoomHandler);
+
+    var container = selection.append('g').attr('class', 'minimap');
+
+    container.call(this.zoom);
+
+    this.frame = container.append('g').attr('class', 'frame');
+
+    this.frame
+      .append('rect')
+      .attr('class', 'background')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .attr('filter', 'url(#minimapDropShadow_qPWKOg)');
+
+    //this.updateMinimapZoomExtents();
   }
 
-  create(): void {
-    const containerHeight = this.workSpace.svg.attr('height');
-    const containerWidth = this.workSpace.svg.attr('width');
-
-    // Calculate height
-    const minimapWidth = parseInt(containerWidth) / this.options.scale;
-    const minimapHeight = parseInt(containerHeight) / this.options.scale;
-
-    // create minimap container
-    this.svg = this.workSpace.self.append('svg').attrs({
-      class: 'minimap minimap-bottom-right',
-      height: minimapHeight,
-      width: minimapWidth
-    });
-
-    this.createZoomViewPort(this.svg);
+  // updates the zoom boundaries based on the current size and scale
+  updateMinimapZoomExtents(): void {
+    var scale = 0;
+    var targetWidth = parseInt(this.target.attr('width'));
+    var targetHeight = parseInt(this.target.attr('height'));
+    var viewportWidth = this.host.width();
+    var viewportHeight = this.host.height();
+    this.zoom.translateExtent([
+      [-viewportWidth / scale, -viewportHeight / scale],
+      [
+        viewportWidth / scale + targetWidth,
+        viewportHeight / scale + targetHeight
+      ]
+    ]);
   }
 
-  /**
-   * Create minimap zoom view port
-   */
-  createZoomViewPort(
-    svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>
-  ): void {
-    const svgHeight = svg.attr('height');
-    const svgWidth = svg.attr('width');
+  zoomHandler() {
+    this.frame.attr('transform', d3.event.transform);
+    // here we filter out the emitting of events that originated outside of the normal ZoomBehavior; this prevents an infinite loop
+    // between the host and the minimap
+    if (
+      d3.event.sourceEvent instanceof MouseEvent ||
+      d3.event.sourceEvent instanceof WheelEvent
+    ) {
+      // invert the outgoing transform and apply it to the host
+      var transform = d3.event.transform;
+      // ordering matters here! you have to scale() before you translate()
+      var modifiedTransform = d3.zoomIdentity
+        .scale(1 / transform.k)
+        .translate(-transform.x, -transform.y);
+      this.host.update(modifiedTransform);
+    }
 
-    this.zoomViewport = svg.append('rect').attrs({
-      class: 'minimap-viewport',
-      height: svgHeight,
-      width: svgWidth,
-      transform: 'translate(0,0)scale(' + this.options.minimapZoomScale + ')',
-      stroke: '#aaaaaa'
-    });
+    this.updateMinimapZoomExtents();
+  }
 
-    var dragPosX = 0;
-    var dragPosY = 0;
+  update(hostTransform) {
+    // invert the incoming zoomTransform; ordering matters here! you have to scale() before you translate()
+    var modifiedTransform = d3.zoomIdentity
+      .scale(1 / hostTransform.k)
+      .translate(-hostTransform.x, -hostTransform.y);
+    // call this.zoom.transform which will reuse the handleZoom method below
+    this.zoom.transform(this.frame, modifiedTransform);
+    // update the new transform onto the minimapCanvas which is where the zoomBehavior stores it since it was the call target during initialization
+    this.container.property('__zoom', modifiedTransform);
 
-    // Add drag behavior
-    const drag = d3.drag().on('drag', () => {
-      dragPosX += d3.event.dx;
-      dragPosY += d3.event.dy;
+    this.updateMinimapZoomExtents();
+  }
 
-      this.zoomViewport.attr(
-        'transform',
-        'translate(' +
-          dragPosX +
-          ',' +
-          dragPosY +
-          ')' +
-          'scale(' +
-          this.options.minimapZoomScale +
-          ')'
-      );
+  render() {
+    // update the placement of the minimap
+    this.container.attr(
+      'transform',
+      'translate(' + this.x + ',' + this.y + ')scale(' + this.minimapScale + ')'
+    );
+    // update the visualization being shown by the minimap in case its appearance has changed
+    var node = this.target.node().cloneNode(true);
+    node.removeAttribute('id');
+    this.base.selectAll('.minimap .panCanvas').remove();
 
-      var translate = [
-        -dragPosX * this.options.scale,
-        -dragPosY * this.options.scale
-      ];
+    d3.select(node).attr('transform', 'translate(0,0)');
+    // keep the minimap's viewport (frame) sized to match the current visualization viewport dimensions
+    this.frame
+      .select('.background')
+      .attr('width', this.width)
+      .attr('height', this.height);
+    this.frame.node().parentNode.appendChild(this.frame.node());
+  }
 
-      this.workSpace.svg.attr('transform', 'translate(' + translate + ')');
-    });
+  setWidth(value) {
+    if (!arguments.length) return this.width;
+    this.width = parseInt(value, 10);
+    return this;
+  }
 
-    this.zoomViewport.call(drag);
+  setHeight(value) {
+    if (!arguments.length) return this.height;
+    this.height = parseInt(value, 10);
+    return this;
+  }
+
+  setX(value) {
+    if (!arguments.length) return this.x;
+    this.x = parseInt(value, 10);
+    return this;
+  }
+
+  setY(value) {
+    if (!arguments.length) return this.y;
+    this.y = parseInt(value, 10);
+    return this;
+  }
+
+  setHost(value) {
+    if (!arguments.length) return this.host;
+    this.host = value;
+    return this;
+  }
+
+  setMinimapScale(value) {
+    if (!arguments.length) return this.minimapScale;
+    this.minimapScale = value;
+    return this;
+  }
+
+  setTarget(value) {
+    if (!arguments.length) return this.target;
+    this.target = value;
+    this.width = parseInt(this.target.attr('width'), 10);
+    this.height = parseInt(this.target.attr('height'), 10);
+    return this;
   }
 }
